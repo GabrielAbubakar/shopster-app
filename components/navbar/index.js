af0ad95002dee0565375a36d2af1d9903ec1242c
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-08-02 20:53:20 +0100

    navbar color based on category

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,15 +1,19 @@
 import Image from 'next/image'
+import { useSelector } from 'react-redux'
 import { Container } from './navbar.styled'
 import Logo from '../../public/assets/logo.svg'
 import Cart from '../../public/assets/cart.svg'
 
 const Navbar = () => {
+
+    const { currentCategory } = useSelector((state => state.persistedReducer.cart))
+
     return (
         <Container>
             <ul>
-                <li>All</li>
-                <li>Tech</li>
-                <li>Clothes</li>
+                <li style={{ color: currentCategory === 'all' && '#05cb05' }}>All</li>
+                <li style={{ color: currentCategory === 'tech' && '#05cb05' }}>Tech</li>
+                <li style={{ color: currentCategory === 'clothes' && '#05cb05' }}>Clothes</li>
             </ul>
 
             <div className='logo'>

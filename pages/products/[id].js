1d1be7925d1ea46dc53e86cd86e7bdcf1ec2b213
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-08-03 17:44:07 +0100

    id js edit for loading screen

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,6 +1,6 @@
 import Navbar from '../../components/navbar'
 import client from '../../graphql/apolloclient'
-import { getAllProducts, getProductsByCategory, getProduct } from '../../graphql/queries'
+import { getAllProducts, getProduct } from '../../graphql/queries'
 
 export async function getStaticPaths() {
     const res = await client.query({
@@ -35,12 +35,25 @@ export async function getStaticProps({ params }) {
 
 const ProductDetails = ({ product }) => {
 
-    console.log(product);
 
     return (
         <div>
             <Navbar />
-            <h1>{product.data.product.name}</h1>
+            {
+                product.loading === true && (
+                    <div>
+                        Loading...
+                    </div>
+                )
+            }
+
+            {
+                product.loading === false && (
+                    <div>
+                        <h1>{product.data.product.name}</h1>
+                    </div>
+                )
+            }
         </div>
     )
 }

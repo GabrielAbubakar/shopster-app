d08d133fdf5292fd5071a0e956de725771592632
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-08-03 12:57:12 +0100

    productCard edit

--- components/productCard/index.js
+++ components/productCard/index.js
@@ -1,23 +1,26 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Cartsvg from '../../public/assets/addCart.svg'
 import { Container } from './productCard.styled'
 
-const ProductCard = ({ gallery, name, addCart, prices }) => {
+const ProductCard = ({ id, gallery, name, addCart, prices }) => {
     return (
-        <Container>
-            <figure>
-                <Image
-                    src={gallery[0]}
-                    alt={name}
-                    width={356}
-                    height={338} />
-            </figure>
-            <h3>{name}</h3>
-            <p>${prices[0].amount}</p>
-            <button>
-                <Image src={Cartsvg} alt="cart" />
-            </button>
-        </Container>
+        <Link href={'/products/' + id}>
+            <Container>
+                <figure>
+                    <Image
+                        src={gallery[0]}
+                        alt={name}
+                        width={356}
+                        height={338} />
+                </figure>
+                <h3>{name}</h3>
+                <p>${prices[0].amount}</p>
+                <button>
+                    <Image src={Cartsvg} alt="cart" />
+                </button>
+            </Container>
+        </Link>
     )
 }
 

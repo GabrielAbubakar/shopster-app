05f0b31e1b6598b61f12537ac5e24cd3b3a994a1
Gabriel Abubakar <gabrielogrima@gmail.com>
2022-08-26 20:26:53 +0100

    graphql api route updated

--- graphql/apolloclient.js
+++ graphql/apolloclient.js
@@ -2,7 +2,7 @@ import { InMemoryCache, ApolloClient, HttpLink } from "@apollo/client";
 import { fetch } from 'cross-fetch'
 
 const client = new ApolloClient({
-    link: new HttpLink({ uri: 'http://localhost:4000/', fetch }),
+    link: new HttpLink({ uri: 'https://scandiwebserve.herokuapp.com/', fetch }),
     cache: new InMemoryCache({
         dataIdFromObject: (o) => (o._id ? `${o.__typename}:${o._id}` : null),
     }),
